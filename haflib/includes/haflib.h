0ef4297106190020433b9a3d2c8ab094b40615b5
corentin <corentin.noel@aliceadsl.fr>
2019-05-20 19:31:58 +0200

    Let's fix the header

--- haflib/includes/haflib.h
+++ haflib/includes/haflib.h
@@ -6,7 +6,7 @@
 /*   By: conoel <conoel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 18:03:06 by conoel            #+#    #+#             */
-/*   Updated: 2019/05/20 13:23:17 by conoel           ###   ########.fr       */
+/*   Updated: 2019/05/20 16:41:16 by conoel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,7 @@ char				*ft_strchr(const char *s, int c);
 char				*ft_strcat(char *s1, const char *s2);
 char				*ft_strncpy(char *dest, const char *src, size_t n);
 void				*ft_memmove(void *dest, const void *src, size_t n);
+char				*ft_strcpy(char *dest, const char *src);
 
 char				*concat(char *s1, char *s2, char *s3);
 
--- src/compile.c
+++ src/compile.c
@@ -6,7 +6,7 @@
 /*   By: conoel <conoel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 11:27:46 by conoel            #+#    #+#             */
-/*   Updated: 2019/05/20 14:45:44 by conoel           ###   ########.fr       */
+/*   Updated: 2019/05/20 19:00:00 by conoel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,22 @@ static char	*param_encoding(t_token *param, int	*len)
 	return (ret);
 }
 
+static char	ocp(t_token_type param, int i)
+{
+	int	ocp;
+
+	ocp = 0;
+	if (param == REG)
+		ocp = 4;
+	else if (param == DIRECT)
+		ocp = 8;
+	else if (param == INDIRECT)
+		ocp = 12;
+	while (i++ < 2)
+		ocp = ocp * 4;
+	return (ocp);
+}
+
 static void	add_line(int fd, t_instruction *actual)
 {
 	char	buff[512];
@@ -59,32 +75,55 @@ static void	add_line(int fd, t_instruction *actual)
 	int		i;
 	int		type;
 
+	ft_bzero(buff, 512);
 	i = 0;
 	len = 1;
 	type = get_type(actual->type);
 	buff[0] = g_op_tab[type].opcode;
-	if (g_op_tab[type].opcode)
+	if (g_op_tab[type].ocp)
 	{
-		buff[1] = 1;
+		buff[1] = 0;
 		len++;
 	}
 	while (i < actual->argc)
 	{
-		ft_strcat(&buff[len], param_encoding(actual->args[i++], &len));
+		param_encoding(actual->args[i], &len);
+		if (g_op_tab[type].ocp)
+			buff[1] += ocp(actual->args[i]->type, i);
+		i++;
 	}
 	write(fd, buff, len);
+}
+
+static void	header(t_token *head, int fd)
+{
+	header_t	*header;
+
+	if (!(header = malloc(sizeof( header_t))))
+		return ;
+	ft_bzero(header, sizeof(header_t));
+	header->magic = COREWAR_EXEC_MAGIC;
+	next_token(&head);
+	ft_strcpy(header->prog_name, head->content);
+	next_token(&head);
+	ft_strcpy(header->comment, head->content);
+	write(fd, header, sizeof(header_t));
 }
 
 int		compile(t_token *head, char *file_name_s)
 {
 	t_instruction	*code;
 	int		fd;
+	char	*tmp;
 
+	if (!(fd = open(tmp , O_CREAT | O_WRONLY | O_TRUNC, 0644)))
+		return (return_("Error: failed to create .cor file (compile, compile.c"));
+	header(head, fd);
 	head = NULL;
 	code = get_instructions(NULL);
 	code = code->next;
-	if (!(fd = open(file_name(file_name_s), O_CREAT | O_WRONLY)))
-		return (return_("Error: failed to create .cor file (compile, compile.c"));
+	tmp = file_name(file_name_s);
+	free(tmp);
 	while (code)
 	{
 		add_line(fd, code);
