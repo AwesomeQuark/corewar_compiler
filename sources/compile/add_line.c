e1b66a39b35cdca780d6fb6cc59f6cb3cf841a8a
corentin <garsvog1@gmail.com>
2019-06-19 21:17:16 +0200

    Compiled

--- sources/compile/add_line.c
+++ sources/compile/add_line.c
@@ -6,7 +6,7 @@
 /*   By: conoel <conoel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 13:49:22 by conoel            #+#    #+#             */
-/*   Updated: 2019/06/19 21:04:13 by conoel           ###   ########.fr       */
+/*   Updated: 2019/06/19 21:16:53 by conoel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,23 @@ static int	get_type(t_token_type type)
 	return (i);
 }
 
+int			get_byte(t_instruction *actual)
+{
+	t_instruction	*head;
+	int				addr;
+
+	addr = 0;
+	head = get_instructions(NULL);
+	while (head != NULL)
+	{
+		if (actual == head)
+			return (addr);
+		addr += head->byte_len;
+		head = head->next;
+	}
+	return (0);
+}
+
 void		translate_indirect(t_token *param, int *len, char *buff)
 {
 	unsigned short	nb;
@@ -41,11 +58,11 @@ void		translate_indirect(t_token *param, int *len, char *buff)
 	return ;
 }
 
-void		translate_label(char *name, int *len, char *buff)
+void		translate_label(char *name, int *len, char *buff, int byte)
 {
 	unsigned short	nb;
 
-	nb = get_label_addr(name);
+	nb = get_label_addr(name) - byte;
 	nb = reverse_bits_s(nb);
 	buff[(*len)++] = nb & 0xFF;
 	nb = nb >> 8;
@@ -53,7 +70,7 @@ void		translate_label(char *name, int *len, char *buff)
 	return ;
 }
 
-void		translate_direct(t_token *param, int *len, char *buff)
+void		translate_direct(t_token *param, int *len, char *buff, int byte)
 {
 	unsigned int	nb;
 	int				i;
@@ -61,7 +78,7 @@ void		translate_direct(t_token *param, int *len, char *buff)
 	i = 0;
 	if (param && param->content && param->content[1] == LABEL_CHAR)
 	{
-		translate_label(&(param->content[2]), len, buff);
+		translate_label(&(param->content[2]), len, buff, byte);
 		return ;
 	}
 	nb = ft_atoi(&(param->content[1]));
@@ -74,12 +91,12 @@ void		translate_direct(t_token *param, int *len, char *buff)
 	return ;
 }
 
-static int	param_encoding(t_token *param, int *len, char *buff)
+static int	param_encoding(t_token *param, int *len, char *buff, int byte)
 {
 	if (param->type == REG)
 		buff[(*len)++] = param->content[1] - '0';
-	if (param->type == DIRECT)  //TODO
-		translate_direct(param, len, buff);
+	if (param->type == DIRECT)
+		translate_direct(param, len, buff, byte);
 	if (param->type == INDIRECT)
 		translate_indirect(param, len, buff);
 	return (TRUE);
@@ -118,7 +135,7 @@ int			add_line(int fd, t_instruction *actual)
 	i = 0;
 	while (i < actual->argc)
 	{
-		if (!(param_encoding(actual->args[i], &len, buff)))
+		if (!(param_encoding(actual->args[i], &len, buff, get_byte(actual))))
 			return (FALSE);
 		if (g_op_tab[type].ocp)
 			buff[1] += ocp(actual->args[i]->type, i);
